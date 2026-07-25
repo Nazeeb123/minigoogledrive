6272c04e667d23131123f03f7a5597da74cc020f
Kmd Nazeeb Ul Zama <kmdnazeebulzama@gmail.com>
2026-07-25 11:39:00 +0530

    Completed JWT authentication

--- src/main/java/com/minidrive/minigoogledrive/controller/UserController.java
+++ src/main/java/com/minidrive/minigoogledrive/controller/UserController.java
@@ -3,6 +3,7 @@ package com.minidrive.minigoogledrive.controller;
 import com.minidrive.minigoogledrive.dto.LoginRequest;
 import com.minidrive.minigoogledrive.model.User;
 import com.minidrive.minigoogledrive.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,21 +13,20 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-
     @PostMapping("/register")
     public User registerUser(@RequestBody User user) {
 
         return userService.registerUser(user);
     }
 
-
     @PostMapping("/login")
     public String loginUser(@RequestBody LoginRequest loginRequest) {
+
+        System.out.println("========== LOGIN API HIT ==========");
 
         return userService.loginUser(
                 loginRequest.getEmail(),
                 loginRequest.getPassword()
         );
     }
-    
 }
--- src/main/java/com/minidrive/minigoogledrive/model/User.java
+++ src/main/java/com/minidrive/minigoogledrive/model/User.java
@@ -11,8 +11,10 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    
     private String username;
+    @Column(unique = true)
+
 
     private String email;
 
