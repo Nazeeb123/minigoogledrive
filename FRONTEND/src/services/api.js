f465d9b2414f476115b866848d7497f46800db0a
Kmd Nazeeb Ul Zama <kmdnazeebulzama@gmail.com>
2026-08-22 12:30:49 +0530

    Connect frontend to deployed backend

--- FRONTEND/src/services/api.js
+++ FRONTEND/src/services/api.js
@@ -1,28 +1,24 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://minigoogledrive-backend.onrender.com",
 });
-
 
 API.interceptors.request.use(
     (config) => {
-
         const token = localStorage.getItem("token");
 
-         console.log("TOKEN:", token);
+        console.log("TOKEN:", token);
 
-        if(token){
+        if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
 
         return config;
     },
-    (error)=>{
+    (error) => {
         return Promise.reject(error);
     }
 );
-
-
 
 export default API;
