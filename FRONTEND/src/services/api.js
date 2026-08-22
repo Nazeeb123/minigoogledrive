2c1e22d2f97da0bf5463e3ff0dc47b0a5fde29ae
Kmd Nazeeb Ul Zama <kmdnazeebulzama@gmail.com>
2026-08-22 17:02:41 +0530

    Fix production backend URL

--- FRONTEND/src/services/api.js
+++ FRONTEND/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://minigoogledrive-backend.onrender.com",
+    baseURL: "https://minigoogledrive-1.onrender.com",
 });
 
 API.interceptors.request.use(
